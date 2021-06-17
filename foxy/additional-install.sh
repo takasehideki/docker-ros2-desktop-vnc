0768f8a4aaa64fbf2aef208232836c577ec105ac
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2021-06-17 20:58:12 +0900

    add install to terminator

---
+++ foxy/additional-install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -eu
+
+CHOOSE_ROS_DISTRO=foxy
+
+# additional install for the seminar
+sudo apt-get update
+sudo apt-get install -y terminator
+sudo apt-get install -y ros-${CHOOSE_ROS_DISTRO}-gazebo-ros-pkgs
+
+set +u
+
+source /opt/ros/${CHOOSE_ROS_DISTRO}/setup.bash
+
