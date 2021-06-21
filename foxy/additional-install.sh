e4936918f8c691eeb61ea3100926bd025842773b
takasehideki <takasehideki@hal.ipc.i.u-tokyo.ac.jp>
2021-06-22 01:58:19 +0900

    additional install for TurtleBot3 environment

--- foxy/additional-install.sh
+++ foxy/additional-install.sh
@@ -3,14 +3,29 @@ set -eu
 
 CHOOSE_ROS_DISTRO=foxy
 
-# additional install for the seminar
+## additional install for the seminar
 sudo apt-get update
 sudo apt-get install -y terminator
-sudo apt-get install -y ros-${CHOOSE_ROS_DISTRO}-gazebo-ros-pkgs
+
+## additional install for TurtleBot3 environment
+## https://emanual.robotis.com/docs/en/platform/turtlebot3/quick-start/#pc-setup
+# Install Gazebo11
+sudo apt-get install -y ros-${CHOOSE_ROS_DISTRO}-gazebo-*
+# Install Cartographer
+sudo apt install -y ros-${CHOOSE_ROS_DISTRO}-cartographer
+sudo apt install -y ros-${CHOOSE_ROS_DISTRO}-cartographer-ros
+# Install Navigation2
+sudo apt install -y ros-${CHOOSE_ROS_DISTRO}-navigation2
+sudo apt install -y ros-${CHOOSE_ROS_DISTRO}-nav2-bringup
+# Install TurtleBot3 Packages
+sudo apt install -y ros-${CHOOSE_ROS_DISTRO}-dynamixel-sdk
+sudo apt install -y ros-${CHOOSE_ROS_DISTRO}-turtlebot3-msgs
+sudo apt install -y ros-${CHOOSE_ROS_DISTRO}-turtlebot3
 
 set +u
 
-source /opt/ros/${CHOOSE_ROS_DISTRO}/setup.bash
-
 grep -F "export ROS_LOCALHOST_ONLY=1" ~/.bashrc ||
 echo "export ROS_LOCALHOST_ONLY=1" >> ~/.bashrc
+
+grep -F "export TURTLEBOT3_MODEL=burger" ~/.bashrc ||
+echo "export TURTLEBOT3_MODEL=burger" >> ~/.bashrc
